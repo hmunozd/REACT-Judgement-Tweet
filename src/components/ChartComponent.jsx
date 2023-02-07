9615991536849e16b3c80246b6a640a8cee0a503
hmunozd <jildo.co@gmail.com>
2023-02-07 12:33:31 -0500

    Se arega tooltip

--- src/components/ChartComponent.jsx
+++ src/components/ChartComponent.jsx
@@ -28,76 +28,6 @@ const ChartComponent = ({ tweetsData }) => {
     ],
   }
   const [bubbleData, setBubbleData] = useState(ubleData);
-  const [dataTweet, setDataTweet] = useState([]);
-
-  const barData = {
-    labels: ['January', 'February', 'March', 'April', 'May'],
-    datasets: [
-      {
-        label: 'Bar Chart',
-        data: [10, 20, 30, 40, 50],
-        backgroundColor: 'rgba(75, 192, 192, 0.6)',
-        borderWidth: 1,
-        borderColor: '#777',
-        hoverBorderWidth: 3,
-        hoverBorderColor: '#000'
-      }
-    ]
-  };
-
-  const lineData = {
-    labels: ['January', 'February', 'March', 'April', 'May'],
-    datasets: [
-      {
-        label: 'Line Chart',
-        data: [10, 20, 30, 40, 50],
-        fill: false,
-        lineTension: 0.1,
-        backgroundColor: 'rgba(75, 192, 192, 0.6)',
-        borderColor: 'rgba(75, 192, 192, 1)',
-        borderCapStyle: 'butt',
-        borderDash: [],
-        borderDashOffset: 0.0,
-        borderJoinStyle: 'miter',
-        pointBorderColor: 'rgba(75, 192, 192, 1)',
-        pointBackgroundColor: '#fff',
-        pointBorderWidth: 1,
-        pointHoverRadius: 5,
-        pointHoverBackgroundColor: 'rgba(75, 192, 192, 1)',
-        pointHoverBorderColor: 'rgba(220, 220, 220, 1)',
-        pointHoverBorderWidth: 2,
-        pointRadius: 1,
-        pointHitRadius: 10
-      }
-    ]
-  };
-
-  const pieData = {
-    labels: ['Positive', 'Negative', 'Neutral', 'Green', 'Purple', 'Orange'],
-    datasets: [
-      {
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)',
-        ],
-        borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)',
-        ],
-        borderWidth: 1,
-      },
-    ]
-  };
 
   const options = {
     responsive: true,
@@ -112,17 +42,11 @@ const ChartComponent = ({ tweetsData }) => {
     },
     tooltip: {
       callbacks: {
-        label: function (context) {
-          let label = context.dataset.label || '';
-
-          if (label) {
-            label += ': ';
-          }
-          if (context.parsed.y !== null) {
-            label += new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(context.parsed.y);
-          }
-          return label;
-        }
+        label: function (tooltipItem, data) {
+          const dataset = data.datasets[tooltipItem.datasetIndex];
+          const label = dataset.label;
+          return label + ': ' + tooltipItem.xLabel + '% negativo, ' + tooltipItem.yLabel + '% positivo';
+        },
       }
     }
   };
